21168fc6ed0125f0439c2d9e3e6305dff9500010
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-04 08:20:30 +0000

    :memo: Strongswanインストールまでを追加

--- envs/config/setup.sh
+++ envs/config/setup.sh
@@ -8,11 +8,39 @@
 set -euo pipefail
 
 # Package Update
-dnf update -y
+apt update -y
 
 # Timezone
 timedatectl set-timezone Asia/Tokyo
 
-# Locale
+# Locale & Keymap
 localectl set-locale LANG=ja_JP.UTF-8
-localectl set-keymap jp-OADG109A
+sed -i 's/^XKBLAYOUT=.*/XKBLAYOUT="jp"/' /etc/default/keyboard
+dpkg-reconfigure -f noninteractive keyboard-configuration # 設定再構築
+setupcon # ttyへ即時反映
+
+# Firewall 無効化
+ufw disable
+
+# IP Forward & rp_filter
+### all: 全インターフェース適用
+### default: インターフェース側で未設定の場合に適用
+### ip_forward = 1 [あるNICで受信したバケットを別NICへ送出する]
+### ip_no_pmtu_disc = 0 [Path MTU Discoveryを無効化]
+### accept_redirects = 0 [ICMPリダイレクトパケットの受入無効化]
+### send_redirects = 0 [ICMPリダイレクトパケットの送出無効化]
+cat <<EOF > /etc/sysctl.d/99-vpn.conf
+net.ipv4.ip_forward = 1
+net.ipv4.ip_no_pmtu_disc=0
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+EOF
+### /etc/sysctl.d/* を適用する
+### sysctl -p は /etc/sysctl.conf のみ反映される
+sysctl --system
+
+# Strongswan install
+apt install strongswan strongswan-swanctl -y
+systemctl enable --now ipsec
